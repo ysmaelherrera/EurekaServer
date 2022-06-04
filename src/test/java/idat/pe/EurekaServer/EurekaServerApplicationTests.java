b968d38c78b64919b567ea934d99d7694da72d00
ysmaelherrera <104236695+ysmaelherrera@users.noreply.github.com>
2022-06-03 23:37:33 -0500

    Add files via upload

---
+++ src/test/java/idat/pe/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.pe.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
